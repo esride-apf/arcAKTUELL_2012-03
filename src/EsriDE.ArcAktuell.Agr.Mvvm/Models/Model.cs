934144f841bc8e414ff5d8d097213b01328b12e3
Alexander Erbe <a.erbe@esri.de>
2012-07-18 09:04:22 +0200

    ale - added a method to canceling queries

--- src/EsriDE.ArcAktuell.Agr.Mvvm/Models/Model.cs
+++ src/EsriDE.ArcAktuell.Agr.Mvvm/Models/Model.cs
@@ -5,79 +5,98 @@ using EsriDE.ArcAktuell.Agr.Mvvm.DomainModels;
 
 namespace EsriDE.ArcAktuell.Agr.Mvvm.Models
 {
-	public class Model : IModel
-	{
-		private QueryTask _queryTaskEvacuationPerimeter;
-		private QueryTask _queryTaskFirePerimeter;
-		private QueryTask _queryTaskPointsOfInterest;
+    public class Model : IModel
+    {
+        private QueryTask _queryTaskEvacuationPerimeter;
+        private QueryTask _queryTaskFirePerimeter;
+        private QueryTask _queryTaskPointsOfInterest;
 
-		public Model()
-		{
-			InitializeQueryComponents();
-		}
+        public Model()
+        {
+            InitializeQueryComponents();
+        }
 
-		#region IModel Members
+        #region IModel Members
 
-		public event Action<int, QueryType> QueryCompleted = delegate { };
+        public event Action<int, QueryType> QueryCompleted = delegate { };
 
-		public void DoQuery(Geometry geometry)
-		{
-			StartQuery(geometry, QueryType.PointsOfInterest);
-			StartQuery(geometry, QueryType.EvacuationPerimeter);
-			StartQuery(geometry, QueryType.FirePerimeter);
-		}
+        public void DoQuery(Geometry geometry)
+        {
+            CancelBusyQueries();
 
-		#endregion
+            StartQuery(geometry, QueryType.PointsOfInterest);
+            StartQuery(geometry, QueryType.EvacuationPerimeter);
+            StartQuery(geometry, QueryType.FirePerimeter);
+        }
 
-		private void InitializeQueryComponents()
-		{
-			_queryTaskPointsOfInterest =
-				new QueryTask(@"http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Fire/Sheep/FeatureServer/0");
-			_queryTaskPointsOfInterest.ExecuteCompleted += QueryTaskExecuteCompleted;
-			_queryTaskPointsOfInterest.Failed += QueryTaskFailed;
+        #endregion
 
-			_queryTaskEvacuationPerimeter =
-				new QueryTask(@"http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Fire/Sheep/FeatureServer/1");
-			_queryTaskEvacuationPerimeter.ExecuteCompleted += QueryTaskExecuteCompleted;
-			_queryTaskEvacuationPerimeter.Failed += QueryTaskFailed;
+        private void InitializeQueryComponents()
+        {
+            _queryTaskPointsOfInterest =
+                new QueryTask(@"http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Fire/Sheep/FeatureServer/0");
+            _queryTaskPointsOfInterest.ExecuteCompleted += QueryTaskExecuteCompleted;
+            _queryTaskPointsOfInterest.Failed += QueryTaskFailed;
 
-			_queryTaskFirePerimeter =
-				new QueryTask(@"http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Fire/Sheep/FeatureServer/2");
-			_queryTaskFirePerimeter.ExecuteCompleted += QueryTaskExecuteCompleted;
-			_queryTaskFirePerimeter.Failed += QueryTaskFailed;
-		}
+            _queryTaskEvacuationPerimeter =
+                new QueryTask(@"http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Fire/Sheep/FeatureServer/1");
+            _queryTaskEvacuationPerimeter.ExecuteCompleted += QueryTaskExecuteCompleted;
+            _queryTaskEvacuationPerimeter.Failed += QueryTaskFailed;
 
-		private void StartQuery(Geometry geometry, QueryType queryType)
-		{
-			var query = new Query
-			            	{
-			            		Geometry = geometry,
-			            		ReturnGeometry = false,
-			            		Where = "true"
-			            	};
+            _queryTaskFirePerimeter =
+                new QueryTask(@"http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Fire/Sheep/FeatureServer/2");
+            _queryTaskFirePerimeter.ExecuteCompleted += QueryTaskExecuteCompleted;
+            _queryTaskFirePerimeter.Failed += QueryTaskFailed;
+        }
 
-			switch (queryType)
-			{
-				case QueryType.PointsOfInterest:
-					_queryTaskPointsOfInterest.ExecuteAsync(query, queryType);
-					break;
-				case QueryType.EvacuationPerimeter:
-					_queryTaskEvacuationPerimeter.ExecuteAsync(query, queryType);
-					break;
-				case QueryType.FirePerimeter:
-					_queryTaskFirePerimeter.ExecuteAsync(query, queryType);
-					break;
-			}
-		}
+        private void StartQuery(Geometry geometry, QueryType queryType)
+        {
+            var query = new Query
+                            {
+                                Geometry = geometry,
+                                ReturnGeometry = false,
+                                Where = "true"
+                            };
 
-		private void QueryTaskExecuteCompleted(object sender, QueryEventArgs args)
-		{
-			QueryCompleted(args.FeatureSet.Features.Count, (QueryType) args.UserState);
-		}
+            switch (queryType)
+            {
+                case QueryType.PointsOfInterest:
+                    _queryTaskPointsOfInterest.ExecuteAsync(query, queryType);
+                    break;
+                case QueryType.EvacuationPerimeter:
+                    _queryTaskEvacuationPerimeter.ExecuteAsync(query, queryType);
+                    break;
+                case QueryType.FirePerimeter:
+                    _queryTaskFirePerimeter.ExecuteAsync(query, queryType);
+                    break;
+            }
+        }
 
-		private void QueryTaskFailed(object sender, TaskFailedEventArgs args)
-		{
-			QueryCompleted(0, (QueryType) args.UserState);
-		}
-	}
+        private void QueryTaskExecuteCompleted(object sender, QueryEventArgs args)
+        {
+            QueryCompleted(args.FeatureSet.Features.Count, (QueryType) args.UserState);
+        }
+
+        private void QueryTaskFailed(object sender, TaskFailedEventArgs args)
+        {
+            QueryCompleted(0, (QueryType) args.UserState);
+        }
+
+        private void CancelBusyQueries()
+        {
+            if (_queryTaskFirePerimeter.IsBusy)
+            {
+                _queryTaskPointsOfInterest.CancelAsync();
+            }
+            if (_queryTaskEvacuationPerimeter.IsBusy)
+            {
+                _queryTaskEvacuationPerimeter.CancelAsync();
+            }
+
+            if (_queryTaskFirePerimeter.IsBusy)
+            {
+                _queryTaskFirePerimeter.CancelAsync();
+            }
+        }
+    }
 }
